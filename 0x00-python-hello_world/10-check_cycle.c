cdd43a05c75e83e212ba11a3c3f7b124db4d8c03
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-02 20:39:02 +0300

    hello world python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* check_cycle - check for loop in linked list
+* @list: head of the linked list
+*
+* Return: 1 if cycled 0 if no cycle
+*/
+
+int check_cycle(listint_t *list)
+{
+
+}
