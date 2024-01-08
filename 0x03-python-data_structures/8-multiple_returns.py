6a9d3d577758300ec634ef2183b0f5a10743b5d9
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-08 11:46:00 +0300

    8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,12 @@
+"""provides multiple returns."""
+
+
 #!/usr/bin/python3
-
-
 def multiple_returns(sentence):
+    """Multiple returns."""
     tuplea = ()
     if len(sentence) == 0:
-        tuplea = tuplea + ("None",)
+        tuplea = 0, "None"
     else:
         tuplea = len(sentence), sentence[0]
     return tuplea
