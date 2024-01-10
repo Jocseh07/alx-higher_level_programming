287559fee77769a773b75135e270373e10fe036e
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-10 13:56:36 +0300

    error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 
 
 def best_score(a_dictionary):
-    new = dict(sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True))
-    return list(new.keys())[0]
+    if a_dictionary:
+        new = dict(sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True))
+        return list(new.keys())[0]
+    return "None"
