1e4f3685dea90accaf2801317f15b1beffd0de76
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-22 16:11:40 +0300

    advanced

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+import sys
+
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
