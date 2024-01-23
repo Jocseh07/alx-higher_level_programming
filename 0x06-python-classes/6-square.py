d849f7c6448d40adee619edb2098647e53211e24
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-23 14:56:06 +0300

    mandatory

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,11 +59,9 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        for _ in range(0, self.__position[1]):
-            print("")
+
+        [print("") for _ in range(0, self.__position[1])]
         for _ in range(0, self.__size):
-            for _ in range(0, self.__position[0]):
-                print(" ", end="")
-            for _ in range(self.__size):
-                print("#", end="")
+            [print(" ", end="") for _ in range(0, self.__position[0])]
+            [print("#", end="") for _ in range(0, self.__size)]
             print("")
