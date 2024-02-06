74d400179cb0de86606e8887989694cbb02d51be
Jocseh07 <josephmkaranja07@gmail.com>
2024-02-06 07:07:47 +0300

    mandatory

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
             name (str): name of parameter.
             value (int): value of parameter.
         """
-        if not type(value) != int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
