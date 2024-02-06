ec11951657a06a4de611b45dab49df9ec789b015
Jocseh07 <josephmkaranja07@gmail.com>
2024-02-06 16:10:57 +0300

    mandatory

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,5 +9,5 @@ def write_file(filename="", text=""):
         filename (str): name of the file.
         text (str): text to write to string.
     """
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
