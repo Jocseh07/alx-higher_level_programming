b540dd4ed4de09675b0b658157bc918127ab3621
Jocseh07 <josephmkaranja07@gmail.com>
2024-02-06 11:43:21 +0300

    mandatory

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,4 +5,10 @@
 class MyInt(int):
     """Inverts == and !=."""
 
-    pass
+    def __eq__(self, value):
+        """set == to !="""
+        return self != value
+
+    def __ne__(self, value):
+        """set != to =="""
+        return self == value
