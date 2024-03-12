233bc1c0be5e62a585df92a9c6e86c480faf1200
Jocseh07 <josephmkaranja07@gmail.com>
2024-03-12 16:56:08 +0300

    sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- DISPLAY TOP 3 CITIES TEMPERATURE DURING july AND aUGUST ORDERED BY TEMP DESCCENDING
 SELECT `city`, AVG('value') AS `avg`
 FROM `temperatures`
-WHERE `month` = 7 OR month = 8
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg` DESC
 LIMIT 3;
