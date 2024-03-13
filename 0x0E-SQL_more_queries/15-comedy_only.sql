6e79b4feebccd2976e935733bb4c5de1c3903985
Jocseh07 <josephmkaranja07@gmail.com>
2024-03-13 18:04:11 +0300

    sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,10 @@
 -- list all comedy shows in database
 SELECT title
 FROM tv_shows
-LEFT JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.show_id
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
-ON tv_shows_genres.genre_id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 GROUP BY title
 ORDER BY title;
