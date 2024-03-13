747767da15b025a7262dd19162647308c7fc6fa4
Jocseh07 <josephmkaranja07@gmail.com>
2024-03-13 17:56:14 +0300

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- create database hbtn_0d_usa and cities table in the database in server
 CREATE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-id INT UNIQUE NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY(id),
