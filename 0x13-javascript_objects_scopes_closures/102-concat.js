c06fc025a2a885e3ff1a44c05ac76a1b5a948ee0
Jocseh07 <josephmkaranja07@gmail.com>
2024-04-09 21:32:57 +0300

    more

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2]).toString;
-const b = fs.readFileSync(process.argv[3]).toString;
+const a = fs.readFileSync(process.argv[2]).toString();
+const b = fs.readFileSync(process.argv[3]).toString();
 fs.writeFileSync(process.argv[4], a, b);
