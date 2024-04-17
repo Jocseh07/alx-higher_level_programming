76031942c7eb2398b085645010e68e7cd807ae64
Jocseh07 <josephmkaranja07@gmail.com>
2024-04-17 09:58:32 +0300

    more

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,7 +9,7 @@ class City(Base):
     """Class with id and name and inherits from Base."""
 
     __tablename__ = "cities"
-    
+
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
