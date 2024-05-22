73effb0537303a01685fd03f2e16a6d4d48d4577
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-23 00:19:27 +0300

    more

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,6 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (err, response, body) => {
   if (err) {
-    console.log(err);
     return;
   }
   const total = JSON.parse(body).results;
