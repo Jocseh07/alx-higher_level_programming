713e8e19b7b24ad111a6f9a9d54ecbf14f3c0642
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-23 00:09:55 +0300

    more

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = "https://swapi-api.alx-tools.io/api/films/" + process.argv[2];
 request(url, (err, response, body) => {
   if (err) {
     console.log(err);
