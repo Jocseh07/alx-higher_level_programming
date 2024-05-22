f1651ea570072713cd8e8dff095977d7d905bccf
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-22 23:47:36 +0300

    more

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
+
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (err, response, body) => {
   if (err) {
@@ -7,14 +8,19 @@ request(url, (err, response, body) => {
     return;
   }
   const total = JSON.parse(body).characters;
-  for (const actor of total) {
-    request(actor, (err, response, body) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const actor = JSON.parse(body).name;
-      console.log(actor);
-    });
-  }
+  printName(total, 0);
 });
+
+function printName (actors, index) {
+  request(actors[index], (err, response, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const actor = JSON.parse(body).name;
+    console.log(actor);
+    if (actors[index + 1]) {
+      printName(actors, index + 1);
+    }
+  });
+}
